1a35a1099161abf16bed01e434c6e1922e7c00a4
mellythibs <37309694+mellythibs@users.noreply.github.com>
2018-05-07 09:02:39 -0400

    Create sprite.js

---
+++ js/sprite.js
@@ -0,0 +1,11 @@
+Game.sprite = function (game) {};
+
+//will make sure the OC can't go through the items
+function furniture(){
+
+};
+
+//not sure where to put this at the current point
+//function door(){}
+
+

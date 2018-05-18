b0364b2c68f4f21459edb7bad8a8c4c241427fae
mellythibs <37309694+mellythibs@users.noreply.github.com>
2018-05-18 08:04:59 -0400

    Update house.js

--- js/house.js
+++ js/house.js
@@ -6,13 +6,13 @@ var layer;
 Game.house.prototype = {
 
 	create: function(){
-		//this.stage.backgroundColor = '#3A5963';
+		this.stage.backgroundColor = '#3A3063';
 
 	this.livingroom  = this.addTileSprite(0,
 	this.game.height = this.game.cache.getImage('livingroom').height,
-	this.game.width,
-	this.cacher.getImage('livingroom').heihgt,
-	'livingroom');
+						this.game.width,
+	this.cacher.getImage('livingroom').height,
+						'livingroom');
 
 	},
 

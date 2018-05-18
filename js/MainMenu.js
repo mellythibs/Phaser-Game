7094c09cb08ac54f54383a15e6fdcc1f51f23ce6
mellythibs <37309694+mellythibs@users.noreply.github.com>
2018-05-18 08:01:20 -0400

    Update MainMenu.js

--- js/MainMenu.js
+++ js/MainMenu.js
@@ -1,31 +1,54 @@
 Game.MainMenu = function(game){
 
+
 };
 
-var start;
+
+var button;
+
 
 Game.MainMenu.prototype  = {
+
+
 	preload: function(){
-		this.load.spritesheet('start', 'assets/start.png');
+
+		this.load.spritesheet('button', 'assets/start.png');
+
 	},
+
 
 	create: function(){
+
 		this.stage.backgroundColor = "#4488AA";
-        button = game.add.button(game.world.centerX, game.world.centerY, 'start', actionOnClick, this, 1, 0, 2); //Not sure if it works perfectly yet
+
+        button = this.add.button(this.world.centerX, this.world.centerY, 'button', this.calls, this, 1, 0, 2); //Not sure if it works perfectly yet
+
         button.anchor.setTo(0.5,0.5);
+
 	},
+
 
 	update: function(){
+
 	},
 
+	
 	calls: function(){
+
 		console.log(this);
+
 		if(OC == true)
+
 		{	this.state.start('OC');}
+
 		else	
+
 		{this.state.start('map');}
+
 	},
+
 };
+
 
 //var OC = false;  // temp. boolean statement 
 
